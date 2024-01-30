041b6b9f72f4b7ff6933e63b094466a41cb28a7c
HMCCS <posthauerjoe@gmail.com>
2024-01-29 20:30:17 -0800

    Code that demostrates how to use fetch and connect front/backend

---
+++ REFWebApp.Server/Controllers/STTController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace REFWebApp.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class STTController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<STTController> _logger;
+
+        public STTController(ILogger<STTController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost(Name = "PostSTT")]
+public IEnumerable<STT> Post([FromBody] STTRequestModel request)
+{
+    string[] text = request.Text;
+
+    return Enumerable.Range(1, 5).Select(index => new STT
+    {
+        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        TemperatureC = Random.Shared.Next(-20, 55),
+        Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+        Text = text[index]
+    })
+    .ToArray();
+}
+
+public class STTRequestModel
+{
+    public string[] Text { get; set; }
+}
+    }
+}
