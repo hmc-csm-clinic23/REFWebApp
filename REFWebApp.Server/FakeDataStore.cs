95331f8c314c9d20e3096903bc3d4a5d31eec704
HMCCS <posthauerjoe@gmail.com>
2024-02-06 20:30:43 -0800

    setup fetch function for S3

--- REFWebApp.Server/FakeDataStore.cs
+++ REFWebApp.Server/FakeDataStore.cs
@@ -18,5 +18,50 @@
             await Task.CompletedTask;
         }
         public async Task<IEnumerable<Product>> GetAllProducts() => await Task.FromResult(_products);
+
+
+        /// <summary>
+        /// Shows how to download an object from an Amazon S3 bucket to the
+        /// local computer.
+        /// </summary>
+        /// <param name="client">An initialized Amazon S3 client object.</param>
+        /// <param name="bucketName">The name of the bucket where the object is
+        /// currently stored.</param>
+        /// <param name="objectName">The name of the object to download.</param>
+        /// <param name="filePath">The path, including filename, where the
+        /// downloaded object will be stored.</param>
+        /// <returns>A boolean value indicating the success or failure of the
+        /// download process.</returns>
+        public static async Task<bool> DownloadObjectFromBucketAsync(
+            IAmazonS3 client,
+            string bucketName,
+            string objectName,
+            string filePath)
+        {
+            // Create a GetObject request
+            var request = new GetObjectRequest
+            {
+                BucketName = bucketName,
+                Key = objectName,
+            };
+
+            // Issue request and remember to dispose of the response
+            using GetObjectResponse response = await client.GetObjectAsync(request);
+
+            try
+            {
+                // Save object to local file
+                await response.WriteResponseStreamToFileAsync($"{filePath}\\{objectName}", true, CancellationToken.None);
+                return response.HttpStatusCode == System.Net.HttpStatusCode.OK;
+            }
+            catch (AmazonS3Exception ex)
+            {
+                Console.WriteLine($"Error saving {objectName}: {ex.Message}");
+                return false;
+            }
+        }
+
+
+
     }
 }
