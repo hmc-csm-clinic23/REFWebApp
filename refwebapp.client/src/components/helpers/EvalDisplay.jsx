25d565725608289cea2a4eb5bf716970f339b252
HMCCS <posthauerjoe@gmail.com>
2024-03-28 00:09:22 -0700

    Added search and reformatted evaluation page

--- refwebapp.client/src/components/helpers/EvalDisplay.jsx
+++ refwebapp.client/src/components/helpers/EvalDisplay.jsx
@@ -1,18 +1,37 @@
+import { React, useState } from "react";
 import { Evals } from "../.././components";
-function EvalDisplay({ stt, metrics, transcriptions }) {
+function EvalDisplay({ stt, metrics, transcriptions, groundTruths, scenarios }) {
   const metricsList = metrics.reduceRight((all, item) => ({[item]: all}), {});
   const transcriptionsList = transcriptions.reduceRight((all, item) => ({[item]: all}), {});
-  console.log(metrics);
-  console.log(transcriptions);
-  console.log(metrics.map((metrics) => metrics));
+  const [search, setSearch] = useState("");
   return (
     <div>
       <h1>{stt}</h1>
-      <div>
-        {metrics.map((metrics, index) => 
+      <input
+        type="text"
+        className="searchAudio"
+        placeholder="Search scenarios"
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div className="evalContainer">
+        <div className="evalItem">
+          <div className="evalText">Metrics</div>
+          <div className="evalText">Transcriptions</div>
+          <div className="evalText">Ground Truths</div>
+        </div>
+        {metrics
+        .map((metrics, i) => ({ metrics, i }))
+        .filter(({ i }) =>
+          search.toLowerCase() === ""
+            ? scenarios[i]
+            : scenarios[i].name.toLowerCase().includes(search.toLowerCase()),
+        )
+        .map(({ metrics, i }) => 
         <Evals
         metrics={metrics}
-        transcriptions={transcriptions[index]}
+        transcriptions={transcriptions[i]}
+        groundTruths={groundTruths[i]}
+        scenarios={scenarios[i]}
         />
         )}
       </div>
--- refwebapp.client/src/components/helpers/Evals.jsx
+++ refwebapp.client/src/components/helpers/Evals.jsx
@@ -1,12 +1,35 @@
-function Evals({ metrics, transcriptions }) {
+import { React, useState } from "react";
+function Evals({ metrics, transcriptions, groundTruths, scenarios }) {
   const metricLabels = ["WER: ", "MER: ", "WIL: ", "SIM: ", "L-DIST: "];
+  const [search, setSearch] = useState("");
   return (
     <div>
-      {metrics.map((metrics, index) => 
-      <>
-        <div>[{metrics.map((metric, i) => [metricLabels[i], metric, i < 4 && ", "])}]</div>
-        <div>{transcriptions[index]}</div>
-      </>
+      <div className="evalItem">
+        <div className="evalScenarioText"></div>
+        <div className="evalScenarioText">{scenarios.name}</div>
+        <div className="evalScenarioText"></div>
+      </div>
+      <div className="evalSearchItem">
+        <input
+          type="text"
+          className="evalText"
+          placeholder="Search ground truths"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      {groundTruths
+      .map((groundTruths, i) => ({ groundTruths, i }))
+      .filter(({ groundTruths }) =>
+        search.toLowerCase() === ""
+          ? groundTruths
+          : groundTruths.toLowerCase().includes(search.toLowerCase()),
+      )
+      .map(({ groundTruths, i }) => 
+      <div className="evalItem">
+        <div className="evalText">[{metrics[i].map((metric, index) => [metricLabels[index], metric, index < 4 && ", "])}]</div>
+        <div className="evalText">{transcriptions[i]}</div>
+        <div className="evalText">{groundTruths}</div>
+      </div>
       )}
     </div>
   );
