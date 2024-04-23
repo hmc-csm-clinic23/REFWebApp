ffbbcf27bec2b90b753dd4e7bd122d6c028d1400
HMCCS <dylancam812@gmail.com>
2024-04-22 20:52:32 -0700

    added testing framework

---
+++ REFTests/TestSTTs.cs
@@ -0,0 +1,31 @@
+using REFWebApp.Server.Model;
+using REFWebApp.Server.Model.STTs;
+
+namespace REFTests
+{
+    public class Tests
+    {
+        private ISTT _googleCloud { get; set; } = null!;
+        private ISTT _deepgram { get; set; } = null!;
+        //private ISTT _amazonTranscribe { get; set; } = null!;
+        private ISTT _gladia { get; set; } = null!;
+        private ISTT _whisper { get; set; } = null!;
+
+        [SetUp]
+        public void Setup()
+        { 
+            _googleCloud = new GoogleCloud();
+            _deepgram = new DeepGram();
+            //_amazonTranscribe = new AmazonTranscribe();
+            _gladia = new Gladia();
+            _whisper = new Whisper();
+
+        }
+
+        [Test]
+        public void TestGoogleCloud()
+        {
+            Assert.Pass();
+        }
+    }
+}
