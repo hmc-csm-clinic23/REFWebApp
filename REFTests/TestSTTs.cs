f4efb5824f593c30a988725acf9cb71e1442f654
Olivia Yin <olivia.yin2003@gmail.com>
2024-04-23 11:31:49 -0700

    tests

--- REFTests/TestSTTs.cs
+++ REFTests/TestSTTs.cs
@@ -7,6 +7,7 @@ namespace REFTests
     {
         private ISTT _googleCloud { get; set; } = null!;
         private ISTT _deepgram { get; set; } = null!;
+        
         //private ISTT _amazonTranscribe { get; set; } = null!;
         private ISTT _gladia { get; set; } = null!;
         private ISTT _whisper { get; set; } = null!;
@@ -26,6 +27,33 @@ namespace REFTests
         public void TestGoogleCloud()
         {
             var output = _googleCloud.Run("C:\\Users\\micro\\source\\repos\\REFWebApp\\REFTests\\REFTest.wav");
+            var metrics = _googleCloud.Metrics(output, "Alright. So I I can take that low five back on the PJT.");
+            Assert.IsNotNull(metrics);
+            Assert.That(metrics.Count, Is.EqualTo(5));
+            Assert.IsNotNull(output);
+        }
+
+        [Test]
+        public void TestDeepGram()
+        {
+            var output = _deepgram.Run("C:\\Users\\micro\\source\\repos\\REFWebApp\\REFTests\\REFTest.wav");
+            var metrics = _deepgram.Metrics(output, "Alright. So I I can take that low five back on the PJT.");
+            Assert.IsNotNull(output);
+        }
+
+        [Test]
+        public void TestGladia()
+        {
+            var output = _gladia.Run("C:\\Users\\micro\\source\\repos\\REFWebApp\\REFTests\\REFTest.wav");
+            var metrics = _gladia.Metrics(output, "Alright. So I I can take that low five back on the PJT.");
+            Assert.IsNotNull(output);
+        }
+
+        [Test]
+        public void TestWhisper()
+        {
+            var output = _whisper.Run("C:\\Users\\micro\\source\\repos\\REFWebApp\\REFTests\\REFTest.wav");
+            var metrics = _whisper.Metrics(output, "Alright. So I I can take that low five back on the PJT.");
             Assert.IsNotNull(output);
         }
 
